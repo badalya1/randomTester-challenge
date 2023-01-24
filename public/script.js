6679d582a574616242ad67fa1b05662c3de96388
David Badalyan <badalyandavo@gmail.com>
2023-01-24 18:50:23 +0800

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,66 @@
+"use strict";
+function resizeCanvas(c) {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+}
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
+let canvas;
+let context;
+let secondsPassed;
+let oldTimeStamp;
+let fps;
+let hue = 0;
+let counter;
+let prevX;
+window.onload = init;
+function init() {
+    canvas = document.getElementById("canvas");
+    if (!canvas)
+        throw new Error("Could not find canvas");
+    resizeCanvas(canvas);
+    context = canvas.getContext("2d");
+    if (!context)
+        throw new Error("Could not find context");
+    counter = new Array(canvas.width).fill(0);
+    // Start the first frame request
+    prevX = canvas.width / 2;
+    window.requestAnimationFrame(gameLoop);
+}
+function gameLoop(timestamp) {
+    // secondsPassed = (timestamp - oldTimeStamp) / 1000;
+    // oldTimeStamp = timestamp;
+    // // Calculate fps
+    // fps = Math.round(1 / secondsPassed);
+    // context.font = "25px Arial";
+    // context.fillStyle = "black";
+    // context.fillText("FPS: " + fps, 10, 30);
+    draw();
+    window.requestAnimationFrame(gameLoop);
+}
+function generate(n) {
+    for (var i = 0; i < n; i++) {
+        let randomX = Math.floor(random(-2, 3));
+        let newX = prevX + randomX;
+        if (newX < 0) {
+            newX += canvas.width;
+        }
+        if (newX > canvas.width) {
+            newX -= canvas.width;
+        }
+        counter[newX] += 1;
+        context.fillRect(newX, counter[newX], 1, 1);
+        prevX = newX;
+    }
+}
+function draw() {
+    // hue%=255
+    context.restore();
+    hue += 1;
+    context.fillStyle = `hsl(${hue}, ${100}%, 50%)`;
+    // console.log(canvas.width)
+    generate(10000);
+    generate(1000);
+    generate(100);
+}
